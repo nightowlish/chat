857d971413d8fca97342e695cfd397e522bc757a
andreea-todor <49142518+andreea-todor@users.noreply.github.com>
2020-03-25 19:17:48 +0200

    Update clientdp.c

--- clientdp.c
+++ clientdp.c
@@ -12,22 +12,23 @@ int fd; //This is the socket file descriptor that will be used to identify the s
 int conn; //This is the connection file descriptor that will be used to distinguish client connections.
 char message[1024] = ""; //This array will store the messages that are sent by the server
 
-void *receiving(void *sockID)
+void *receiving(void *sockID) //functia prin care desface ce primeste de la server
 {
 	int socket = *((int *) sockID);
 
 	while(1)
 	{
 		char data[1024];
-		int read = recv(socket, data, 1024, 0);
+		int read = recv(socket, data, 1024, 0); //while(1) e pentru functia asta
+		//asteapta pana primeste de la server
 		data[read] = '\0';
-		if(strlen(data) != 0)
+		if(strlen(data) != 0) //daca a primit ceva
 		{
-			uint16_t us_len = data[0];
-			char username[16];
+			uint16_t us_len = data[0]; //cate caractere are username-ul
+			char username[16]; //pt a stii de la care dintre clienti a venit mesajul prin server
 			strncpy(username, data + 1, us_len - 1);
 			username[1 + us_len] = '\0';
-			printf("%s: %s\n", username, data + 1 + us_len);
+			printf("%s: %s\n", username, data + 1 + us_len + 1);
 			memset(username, 0, sizeof username);
 		}
 		memset(data, 0, sizeof data);
@@ -48,7 +49,7 @@ int main()
 	while(1) 
 	{
     		//printf("Enter a message: ");
-    		fgets(message, 1024, stdin);
+    		fgets(message, 1024, stdin); //ia mesajul clientului acesta
 		//scanf("%s", message);
 
 		/*char command[7];
@@ -59,7 +60,7 @@ int main()
 			char user[16];
 			strncpy(user, message + 8, 16);
 		}*/
-    		send(fd, message, strlen(message), 0);
+    		send(fd, message, strlen(message), 0); //si il trimite la server
 		//strcpy(message, "");
 		//An extra breaking condition can be added here (to terminate the while loop)
 	}
